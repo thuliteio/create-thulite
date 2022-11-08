906383b88e04d975c1a58d5cef434a58d059ef0d
Henk Verlinde <henk@ventizo.com>
2022-11-08 09:11:00 +0100

    feat: initial commit

---
+++ template-bs-starter/config/postcss.config.js
@@ -0,0 +1,35 @@
+const autoprefixer = require('autoprefixer');
+const purgecss = require('@fullhuman/postcss-purgecss');
+const whitelister = require('purgecss-whitelister');
+
+module.exports = {
+  plugins: [
+    autoprefixer(),
+    purgecss({
+      content: [
+        // './node_modules/@hyas/core/layouts/**/*.html',
+        './node_modules/@hyas/themes-starter-bootstrap/layouts/**/*.html',
+        // './layouts/**/*.html',
+        './content/**/*.md',
+      ],
+      safelist: [
+        'lazyloaded',
+        'table',
+        'thead',
+        'tbody',
+        'tr',
+        'th',
+        'td',
+        'h1',
+        'h2',
+        'h3',
+        'alert-link',
+        ...whitelister([
+          // './node_modules/@hyas/core/assets/scss/app.scss',
+          './node_modules/@hyas/themes-starter-bootstrap/assets/scss/components/_alerts.scss',
+          './node_modules/@hyas/themes-starter-bootstrap/assets/scss/components/_code.scss',
+        ]),
+      ],
+    }),
+  ],
+}

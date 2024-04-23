6e24327419ba5a29e52e5f9417ab0ebedb00fa61
Henk Verlinde <henk@henkverlinde.com>
2024-04-23 14:09:04 +0200

    Update for create-vite 5.2.3

--- src/index.ts
+++ src/index.ts
@@ -132,6 +132,10 @@ async function init() {
     'projectName' | 'overwrite' | 'packageName' | 'framework' | 'variant'
   >
 
+  prompts.override({
+    overwrite: argv.overwrite,
+  })
+
   try {
     result = await prompts(
       [
@@ -146,18 +150,33 @@ async function init() {
         },
         {
           type: () =>
-            !fs.existsSync(targetDir) || isEmpty(targetDir) ? null : 'confirm',
+            !fs.existsSync(targetDir) || isEmpty(targetDir) ? null : 'select',
           name: 'overwrite',
           message: () =>
             (targetDir === '.'
               ? 'Current directory'
               : `Target directory "${targetDir}"`) +
-            ` is not empty. Remove existing files and continue?`
+            ` is not empty. Please choose how to proceed:`,
+          initial: 0,
+          choices: [
+            {
+              title: 'Remove existing files and continue',
+              value: 'yes',
+            },
+            {
+              title: 'Cancel operation',
+              value: 'no',
+            },
+            {
+              title: 'Ignore files and continue',
+              value: 'ignore',
+            },
+          ],
         },
         {
           type: (_, { overwrite }: { overwrite?: boolean }) => {
             if (overwrite === false) {
-              throw new Error(red('✖') + ' Operation cancelled')
+              throw new Error(red('×') + ' Operation cancelled')
             }
             return null
           },
@@ -207,7 +226,7 @@ async function init() {
       ],
       {
         onCancel: () => {
-          throw new Error(red('✖') + ' Operation cancelled')
+          throw new Error(red('×') + ' Operation cancelled')
         }
       }
     )
@@ -221,7 +240,7 @@ async function init() {
 
   const root = path.join(cwd, targetDir)
 
-  if (overwrite) {
+  if (overwrite === 'yes') {
     emptyDir(root)
   } else if (!fs.existsSync(root)) {
     fs.mkdirSync(root, { recursive: true })
@@ -239,17 +258,26 @@ async function init() {
 
   if (customCommand) {
     const fullCustomCommand = customCommand
-      .replace('TARGET_DIR', targetDir)
-      .replace(/^npm create/, `${pkgManager} create`)
+      .replace(/^npm create /, () => {
+        // `bun create` uses it's own set of templates,
+        // the closest alternative is using `bun x` directly on the package
+        if (pkgManager === 'bun') {
+          return 'bun x create-'
+        }
+        return `${pkgManager} create `
+      })
       // Only Yarn 1.x doesn't support `@version` in the `create` command
       .replace('@latest', () => (isYarn1 ? '' : '@latest'))
       .replace(/^npm exec/, () => {
-        // Prefer `pnpm dlx` or `yarn dlx`
+        // Prefer `pnpm dlx`, `yarn dlx`, or `bun x`
         if (pkgManager === 'pnpm') {
           return 'pnpm dlx'
         }
         if (pkgManager === 'yarn' && !isYarn1) {
           return 'yarn dlx'
+        }
+        if (pkgManager === 'bun') {
+          return 'bun x'
         }
         // Use `npm exec` in all other cases,
         // including Yarn 1.x and other custom npm clients.
@@ -257,8 +285,10 @@ async function init() {
       })
 
     const [command, ...args] = fullCustomCommand.split(' ')
-    const { status } = spawn.sync(command, args, {
-      stdio: 'inherit'
+    // we replace TARGET_DIR here because targetDir may include a space
+    const replacedArgs = args.map((arg) => arg.replace('TARGET_DIR', targetDir))
+    const { status } = spawn.sync(command, replacedArgs, {
+      stdio: 'inherit',
     })
     process.exit(status ?? 0)
   }
@@ -293,9 +323,14 @@ async function init() {
 
   write('package.json', JSON.stringify(pkg, null, 2))
 
+  const cdProjectName = path.relative(cwd, root)
   console.log(`\nDone. Now run:\n`)
   if (root !== cwd) {
-    console.log(`  cd ${path.relative(cwd, root)}`)
+    console.log(
+      `  cd ${
+        cdProjectName.includes(' ') ? `"${cdProjectName}"` : cdProjectName
+      }`,
+    )
   }
   switch (pkgManager) {
     case 'yarn':
